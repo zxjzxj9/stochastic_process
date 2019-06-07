680c730b09bd73331dae43b3089934c4c2ebcac3
Victor Zhang <zxjzxj9@gmail.com>
2019-06-08 00:01:39 +0800

    Add parameters to vanilla option

--- quant/vanilla_option.h
+++ quant/vanilla_option.h
@@ -5,9 +5,35 @@
 #ifndef NUMERICAL_QUANT_PRACTICE_VANILLA_OPTION_H
 #define NUMERICAL_QUANT_PRACTICE_VANILLA_OPTION_H
 
-
+// Use Black-Scholes model to compute option put/call price
 class vanilla_option {
+public:
+    /*
+     * Strike price: K
+     * Interest rate: r
+     * Option time: T
+     * Underlying asset price: S
+     * Variance of asset price: sigma
+     */
+    vanilla_option(double K, double r, double T, double S, double sigma)
+            : K(K), r(r), T(T), S(S), sigma(sigma) {
 
+    }
+
+    vanilla_option(){}
+
+    // copy constructor
+    vanilla_option& operator=(const vanilla_option& v) {
+        K = v.K;
+        r = v.r;
+        T = v.T;
+        S = v.S;
+        sigma = v.sigma;
+        return *this;
+    }
+
+private:
+    double K, r, T, S, sigma;
 };
 
 
