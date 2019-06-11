acefe2fe1feeedce1b24d1e7ee3e070579e8eee2
Victor Zhang <zxjzxj9@gmail.com>
2019-06-11 23:55:43 +0800

    Add main.cpp testing code

--- quant/main.cpp
+++ quant/main.cpp
@@ -2,6 +2,11 @@
 // Created by Victor Zhang on 2019/6/8.
 //
 
+#include <iostream>
+#include "vanilla_option.h"
+
 int main() {
+    vanilla_option option(100, 0.05, 1.0, 100, 0.2);
+    std::cout<<"Put price: "<< option.calc_put_price()<<" Call price: "<< option.calc_call_price()<<std::endl;
     return 0;
 }
