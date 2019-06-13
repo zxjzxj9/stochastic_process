bb6078568a0293ccf4b0b31200d2f72f361ec296
Victor Zhang <zxjzxj9@gmail.com>
2019-06-13 23:55:08 +0800

    fix the bug of erf function

--- quant/vanilla_option.cpp
+++ quant/vanilla_option.cpp
@@ -21,16 +21,22 @@ vanilla_option::vanilla_option(const vanilla_option& v) {
     sigma = v.sigma;
 }
 
+const double invsqrt2 = 0.70710678118;
+
+inline double cdf(double x) {
+    return 0.5*(erf(invsqrt2*x)+1);
+}
+
 double vanilla_option::calc_call_price() {
     double sigma_sqrt_T = sigma*sqrt(T);
     double d1 = (log(S/K)+(r+0.5*sigma*sigma)*T)/sigma_sqrt_T;
     double d2 = (log(S/K)+(r+0.5*sigma*sigma)*T)/sigma_sqrt_T - sigma_sqrt_T;
-    return S*erf(d1) - K*exp(-r*T)*erf(d2);
+    return S*cdf(d1) - K*exp(-r*T)*cdf(d2);
 }
 
 double vanilla_option::calc_put_price() {
     double sigma_sqrt_T = sigma*sqrt(T);
     double d1 = (log(S/K)+(r+0.5*sigma*sigma)*T)/sigma_sqrt_T;
     double d2 = (log(S/K)+(r+0.5*sigma*sigma)*T)/sigma_sqrt_T - sigma_sqrt_T;
-    return K*exp(-r*T)*erf(-d2) - S*erf(-d1);
+    return -S*cdf(-d1) + K*exp(-r*T)*cdf(-d2) ;
 }
