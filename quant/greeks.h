671c30ab2c086fe3f947a6efd55bd89aa741d821
Victor Zhang <zxjzxj9@gmail.com>
2019-06-19 22:43:20 +0800

    Add delta class to greeks

--- quant/greeks.h
+++ quant/greeks.h
@@ -12,9 +12,19 @@ public:
     greeks(double K, double r, double T, double S, double sigma)
             : K(K), r(r), T(T), S(S), sigma(sigma) {
     }
+
+    greeks();
+
+    virtual double call_price();
+    virtual double put_price();
+    virtual double mc_call_price();
+    virtual double mc_put_price();
 private:
     double K, r, T, S, sigma;
 };
 
+class delta: public greeks {
+
+};
 
 #endif //NUMERICAL_QUANT_PRACTICE_GREEKS_H
