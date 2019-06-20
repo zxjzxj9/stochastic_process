75c6f8c0015b46dea7abc4661fac365595316efc
Victor Zhang <zxjzxj9@gmail.com>
2019-06-20 22:49:23 +0800

    Add some code to delta

--- quant/greeks.h
+++ quant/greeks.h
@@ -15,16 +15,26 @@ public:
 
     greeks();
 
+    virtual double call_price() = 0;
+    virtual double put_price() = 0;
+    virtual double mc_call_price() = 0;
+    virtual double mc_put_price() = 0;
+private:
+    double K, r, T, S, sigma;
+};
+
+class delta: public greeks {
+public:
+    delta(double K, double r, double T, double S, double sigma)
+            : K(K), r(r), T(T), S(S), sigma(sigma) {
+    }
+
     virtual double call_price();
     virtual double put_price();
     virtual double mc_call_price();
     virtual double mc_put_price();
 private:
     double K, r, T, S, sigma;
-};
-
-class delta: public greeks {
-
 };
 
 #endif //NUMERICAL_QUANT_PRACTICE_GREEKS_H
